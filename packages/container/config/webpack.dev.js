c77066f005285227eca5cacef5d0588679b9d001
Andreas Steffensen <andreas.steffensen89@gmail.com>
2024-05-30 20:50:02 +0200

    monorepo start servers works but dotenv wont work yet

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,20 +2,24 @@ const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env files
+const env = dotenv.config().parsed;
 
 const devConfig = {
   mode: "development",
   devServer: {
     port: 8080,
-    historyApiFallback: {
-      index: "index.html",
-    },
+    historyApiFallback: true,
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        marketing: `marketing@${
+          env.REMOTE_ENTRY_URL || "http://localhost:8081/remoteEntry.js"
+        }`,
       },
       shared: packageJson.dependencies,
     }),
